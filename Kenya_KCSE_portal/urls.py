22d2863c7e855b0cbca69c94c717c670815fb1c6
steve-ongera <gadafisteve001@gmail.com>
2024-11-28 14:20:18 +0300

    added all relevant html templates

--- Kenya_KCSE_portal/urls.py
+++ Kenya_KCSE_portal/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('kcse_portal.urls')),  # Main application URLs
+    path('', include('portal.urls')),  # Main application URLs
     path('accounts/', include('django.contrib.auth.urls')),  # Additional auth URLs
 ]
 
